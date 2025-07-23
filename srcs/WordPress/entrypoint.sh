38088a74f9d7fdf387288eb7a57e1bce29ec4cf3
xray <xrayxv9@gmail.com>
2025-07-23 10:20:24 +0200

    aggrandissement de mon espace

--- srcs/WordPress/entrypoint.sh
+++ srcs/WordPress/entrypoint.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 
-wp core install --url="http://cmorel.42.fr" --title="Joy" --admin_user="xray" --admin_password="xray" --admin_email="xrayxv.5@gmail.com" --locale=en_US
+sudo -u www -i -- wp core install --url="http://cmorel.42.fr" --title="Joy" --admin_user="xray" --admin_password="xray" --admin_email="xrayxv.5@gmail.com" --locale=en_US --path /var/www/html
 sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /etc/php/8.3/fpm/pool.d/www.conf
 
 exec $@
